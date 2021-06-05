a1eb479af55f97b84d0d5fdba5a271540099af8f
Vason-err <vasya.vedan@gmail.com>
2021-06-05 23:49:24 +0300

    fix migrations

--- db/schema.rb
+++ db/schema.rb
@@ -42,9 +42,11 @@ ActiveRecord::Schema.define(version: 2021_06_04_142614) do
     t.string "title", null: false
     t.integer "level", default: 0, null: false
     t.bigint "category_id"
+    t.bigint "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["category_id"], name: "index_tests_on_category_id"
+    t.index ["user_id"], name: "index_tests_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -58,4 +60,5 @@ ActiveRecord::Schema.define(version: 2021_06_04_142614) do
   add_foreign_key "answers", "questions"
   add_foreign_key "questions", "tests"
   add_foreign_key "tests", "categories"
+  add_foreign_key "tests", "users"
 end
