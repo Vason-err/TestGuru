0cb01cbbbd67b2616711c50c86f7eea3713ad065
Vason-err <vasya.vedan@gmail.com>
2021-06-09 14:11:54 +0300

    Class method for test

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,2 +1,8 @@
 class Test < ApplicationRecord
+  def self.titles(category_title)
+    joins(:category)
+      .where(categories: { title: category_title })
+      .order(title: :desc)
+      .pluck(:title)
+  end
 end
