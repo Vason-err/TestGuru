c817516d30fc43bfdb9f2c56de0366ecdb3b4455
Vason-err <vasya.vedan@gmail.com>
2021-06-09 22:13:53 +0300

    Fix migrations

--- db/migrate/20210609184101_create_results.rb
+++ db/migrate/20210609184101_create_results.rb
@@ -1,7 +1,9 @@
 class CreateResults < ActiveRecord::Migration[6.1]
   def change
     create_table :results do |t|
-      t.integer :grade
+      t.integer :grade, null: false
+      t.references :test, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
 
       t.timestamps
     end
