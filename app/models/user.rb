5d53e60555dc61f9eba2afb81b305cf09c0592ee
Vason-err <vasya.vedan@gmail.com>
2021-06-09 22:23:19 +0300

    Add methods to user and test

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  def tests_by_level(level)
+    t_ids = []
+    t_ids << results.select(test_id).distinct.where(user_id: id)
+    tests.where(level: level, test_id: t_ids)
+  end
 end
