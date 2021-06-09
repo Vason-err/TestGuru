b45a2106de9502831781f77624ab6a90e0d560fa
Vason-err <vasya.vedan@gmail.com>
2021-06-09 15:48:44 +0300

    fix test level method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
   def tests_by_level(level)
-    tests.where(level: level)
+    tests.where(level: level, test_id: results.select(test_id).distinct.where(user_id: id))
   end
 end
