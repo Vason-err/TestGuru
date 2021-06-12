4d0710dfd9374d7c8cb85b50e1b5f4964059455a
Vason-err <vasya.vedan@gmail.com>
2021-06-12 13:59:09 +0300

    Adding associations to all models

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,4 +1,9 @@
 class Test < ApplicationRecord
+  belongs_to :category
+  belongs_to :author, class_name: "User"
+  has_many :questions
+  has_many :results
+  has_many :users, through: :results
   def self.titles_by_category(category)
     self.joins("LEFT JOIN categories ON tests.category_id = categories.id").where("categories.title = ?", category).order(title: :desc).pluck(:title)
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  has_many :results
+  has_many :tests
+  has_many :created_tests, class_name: "Test", foreign_key: "author_id"
   def tests_by_level(level)
     Test.joins("LEFT JOIN results ON tests.id = results.test_id").where("results.user_id = ? and tests.level = ?", id, level)
   end
