07f8aa5a77e0baa4e447e40e6e07ecfb01309698
Vason-err <vasya.vedan@gmail.com>
2021-06-12 17:21:48 +0300

    Fix models using style guide for macro-style methods

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,9 +1,11 @@
 class Test < ApplicationRecord
   belongs_to :category
   belongs_to :author, class_name: "User"
+
   has_many :questions, dependent: :destroy
   has_many :results, dependent: :restrict_with_exception
   has_many :users, through: :results
+
   def self.titles_by_category(category_title)
     joins(:category)
         .where(categories: { title: category_title })
