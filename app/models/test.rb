3f48b1300f732bbdebf1b8379e3f1a3305e05c99
Vason-err <vasya.vedan@gmail.com>
2021-06-12 17:16:02 +0300

    simplify user and test model methods using associations and adding dependencies to category, question, test and user associations

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,12 +1,12 @@
 class Test < ApplicationRecord
   belongs_to :category
   belongs_to :author, class_name: "User"
-  has_many :questions
-  has_many :results
+  has_many :questions, dependent: :destroy
+  has_many :results, dependent: :restrict_with_exception
   has_many :users, through: :results
-  def self.titles_by_category(category)
-    self.joins("JOIN categories ON tests.category_id = categories.id")
-        .where("categories.title = ?", category)
+  def self.titles_by_category(category_title)
+    joins(:category)
+        .where(categories: { title: category_title })
         .order(title: :desc)
         .pluck(:title)
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
-  has_many :results
-  has_many :tests
-  has_many :created_tests, class_name: "Test", foreign_key: "author_id"
+  has_many :results, dependent: :destroy
+  has_many :tests, through: :results
+  has_many :created_tests, class_name: "Test", foreign_key: "author_id", dependent: :nullify
   def tests_by_level(level)
-    Test.joins("LEFT JOIN results ON tests.id = results.test_id").where("results.user_id = ? and tests.level = ?", id, level)
+    tests.where(level: level)
   end
 end
