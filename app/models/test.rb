57f3ba8b8f3aba3457364a806ddc76259bf245da
Vason-err <vasya.vedan@gmail.com>
2021-06-15 14:07:21 +0300

    Define scopes-methods for category model(order) and for test model(levels: easy, medium etc)

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,10 +1,22 @@
 class Test < ApplicationRecord
+  MIN_LEVEL = 0
+  LEVELS = {
+    easy: MIN_LEVEL..1,
+    medium: 2..4,
+    hard: 5..Float::INFINITY
+  }.freeze
+
   belongs_to :category
   belongs_to :author, class_name: "User"
 
   has_many :questions, dependent: :destroy
   has_many :results, dependent: :restrict_with_exception
   has_many :users, through: :results
+
+  scope :level, -> (level) { where(level: level) }
+  scope :easy, -> { level(LEVELS[:easy]) }
+  scope :medium, -> { level(LEVELS[:medium]) }
+  scope :hard, -> { level(LEVELS[:hard]) }
 
   def self.titles_by_category(category_title)
     joins(:category)
