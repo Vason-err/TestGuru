e8b6c965ed8bbc1d45f6dc3c87b0cb5c30d62e51
Vason-err <vasya.vedan@gmail.com>
2021-06-15 14:47:26 +0300

    Add validation for amount of answers for question(1 to 4)

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -4,4 +4,11 @@ class Answer < ApplicationRecord
   scope :correct, -> { where(correct: true) }
 
   validates :body, presence: true
+  validate :validate_question_answers_amount, on: :create
+
+  def validate_question_answers_amount
+    return if question.answers.count < Question::MAX_ANSWERS_AMOUNT
+
+    errors.add(:question, 'Amount of answers for question should be from 1 to 4')
+  end
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,5 @@
 class Question < ApplicationRecord
+  MAX_ANSWERS_AMOUNT = 4
   belongs_to :test
 
   has_many :answers, dependent: :destroy
