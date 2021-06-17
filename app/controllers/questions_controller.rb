e2405a1a296f0a76af14deb37544ea1209bd59f5
Vason-err <vasya.vedan@gmail.com>
2021-06-17 14:46:08 +0300

    Define for questions controller methods for viewing list of questions of specific test and specific questions of test

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,2 +1,19 @@
 class QuestionsController < ApplicationController
+  def index
+    render json: @test.questions
+  end
+
+  def show
+    render json: @question
+  end
+
+  private
+
+  def find_test
+    @test = Test.find(params[:test_id])
+  end
+
+  def find_question
+    @question = Question.find(params[:id])
+  end
 end
