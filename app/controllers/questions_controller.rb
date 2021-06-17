62f1b308447edaf28d0a556a3c6a79a8d672e098
Vason-err <vasya.vedan@gmail.com>
2021-06-17 15:17:30 +0300

    Define for questions controller methods for creating new question, create routes, create html.erb file for this method

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -7,6 +7,20 @@ class QuestionsController < ApplicationController
     render json: @question
   end
 
+  def new
+    @question = @test.questions.new
+  end
+
+  def create
+    @question = @test.questions.new(question_params)
+    if @question.save
+      redirect_to question_path(@question)
+      render plain: 'Question created'
+    else
+      render :new
+    end
+  end
+
   private
 
   def find_test
@@ -15,5 +29,9 @@ class QuestionsController < ApplicationController
 
   def find_question
     @question = Question.find(params[:id])
+  end
+
+  def question_params
+    params.require(:question).permit(:body)
   end
 end
