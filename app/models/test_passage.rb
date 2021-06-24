7861f2229afa6c89f815f86108f40179aa4d2f12
Vason-err <vasya.vedan@gmail.com>
2021-06-25 02:21:33 +0300

    Define for test passage model methods for accepting correct questions, result counting and success of passing the test and also for checking if answer correct

--- app/models/test_passage.rb
+++ app/models/test_passage.rb
@@ -1,7 +1,30 @@
 class TestPassage < ApplicationRecord
+  SUCCESS_PERCENT = 85
+
   belongs_to :user
   belongs_to :test
   belongs_to :current_question, class_name: "Question", optional: true
+
+  def current_question_number
+    test.questions.order(:id).where('id < ?', current_question.id).count + 1
+  end
+
+  def accept!(answer_ids)
+    self.correct_questions += 1 if correct_answer?(answer_ids)
+    save!
+  end
+
+  def result
+    (correct_questions.to_f / test.questions.count * 100).round
+  end
+
+  def success_result?
+    result >= SUCCESS_PERCENT
+  end
+
+  def completed?
+    current_question.nil?
+  end
 
   private
 
