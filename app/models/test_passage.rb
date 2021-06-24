b10206e5a7bd2e7bb743a154a7d50271478fe995
Vason-err <vasya.vedan@gmail.com>
2021-06-25 00:51:30 +0300

    Add associations to test_passage, rewrite existing associations in test and user model, define method for correct answer

--- app/models/test_passage.rb
+++ app/models/test_passage.rb
@@ -1,2 +1,15 @@
 class TestPassage < ApplicationRecord
+  belongs_to :user
+  belongs_to :test
+  belongs_to :current_question, class_name: "Question", optional: true
+
+  private
+
+  def correct_answer?(answer_ids)
+    correct_answers.ids.sort == Array(answer_ids).map(&:to_i).sort
+  end
+
+  def correct_answers
+    current_question.answers.correct
+  end
 end
