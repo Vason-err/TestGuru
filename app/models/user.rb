14558deac37c153921a2a4ca45dada832c997489
Vason-err <vasya.vedan@gmail.com>
2021-06-28 01:13:46 +0300

    Add validation for email uniqueness and format

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   has_many :tests, through: :test_passages
   has_many :created_tests, class_name: "Test", foreign_key: "author_id", dependent: :nullify
 
-  validates :email, presence: true
+  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
 
   def tests_by_level(level)
     tests.where(level: level)
