366330171fd36400e78c33df4cf99bcfef28ee06
Vason-err <vasya.vedan@gmail.com>
2021-07-17 14:32:02 +0300

    Add routes for badges, add controllers for badges, add views for badges

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,9 @@ module ApplicationHelper
   def github_url(author, repo)
     link_to 'GitHub', "https://github.com/#{author}/#{repo}", target: '_blank'
   end
+
+  def active_link_to(text, path)
+    classes += 'active' if current_page?(path)
+    link_to text, path, class: classes
+  end
 end
